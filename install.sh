c9add87a4cb746304a2f157aa55d317aa2c5344c
firepick1 (localhost) <karl@firepick.org>
2015-10-18 08:56:11 -0700

    ...

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo
+echo "CMD	: `pwd`/prereq.sh"
+echo "INFO	: Installing prerequisites"
+echo "INFO	: `date`"
+
+if [ "$SUDO_USER" != "" ]; then
+  echo "ERROR	: This script must be run by non-root user"
+  echo "TRY	:   ./prereq.sh"
+  exit -1
+fi
+
+read -p "WARN	: This script invokes sudo to change your system. Type \"y\" to proceed: " SUDOOK
+if [ "$SUDOOK" != "y" ]; then
+	exit -1;
+fi
+
+
+if [ "$(type -p firestep)" == "" ]; then
+	echo "INFO	: Installing firestep..."
+	if [ ! -e FireStep ]; then
+		git clone https://github.com/firepick1/FireStep
+	fi
+	pushd FireStep
+	./build
+	sudo make install
+	popd
+fi
+
+if [ "$(type -p node)" == "" ]; then
+	echo "INFO	: Installing nodejs"
+	sudo apt-get install -y nodejs
+fi
+
+echo "DONE	: prerequisites installed."
